13fac968bb30c4cf7a90151167e7d49d9051cacd
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-07 10:58:42 -0700

    Add command and version

---
+++ commands/add.py
@@ -0,0 +1,82 @@
+from commands.base_command import BaseCommand
+import utils
+import requests
+from time import sleep
+import settings
+from bot_token import KEY
+
+class Add(BaseCommand):
+    def __init__(self):
+        description = ("This will add a script to the script hub on the website")
+        params = None
+        super().__init__(description, params)
+
+    async def handle(self, params, message, client):
+
+        channel = message.channel
+
+        def check(m):
+            return m.author.id == message.author.id and m.channel == channel
+
+        msg = "Hello, Please enter the name of the script\n"
+        await channel.send(msg)
+        log = msg
+
+        name = await client.wait_for("message", check=check, timeout=600.0)
+        log += f"<@!{message.author.id}>: {name.content}\n"
+
+        msg = "Please enter the description of the script\n"
+        await channel.send(msg)
+        log += msg
+
+        description = await client.wait_for("message", check=check, timeout=600.0)
+        log += f"<@!{message.author.id}>: {description.content}\n"
+
+        msg = "Please enter the code for the script\n" # , If you have to put a roblox username, then put in HazimosKeySomething as the roblox username\n"
+        await channel.send(msg)
+        log += msg
+
+        code = await client.wait_for("message", check=check, timeout=600.0)
+        log += f"<@!{message.author.id}>: {code.content}\n"
+
+        json_code = {
+            "name": name.content,
+            "description": description.content,
+            "script": code.content,
+            "key": KEY
+        }
+
+        json_msg = f"<@&821781075364675584>\n<@!{message.author.id}> just submitted a request\nname: {json_code['name']}\ndescription: {json_code['description']}\ncode: {json_code['script']}\n------------------------------------------------------------------------"
+
+        msg = "Thank you for submiting a script to the script hub. Your submission will be revied. You will receive a notification if you have been accepted or denied"
+        await message.channel.send(msg)
+        log += msg
+        await utils.log(client, log)
+
+        msg = await client.guilds[0].get_channel(823448966275530802).send(json_msg)
+        await msg.add_reaction(utils.get_emoji(":white_check_mark:"))
+        await msg.add_reaction(utils.get_emoji(":no_entry_sign:"))
+
+        def check(reaction, user):
+            return user.bot == False
+
+        reaction = await client.wait_for("reaction_add", check=check)
+        if reaction[0].emoji == "✅":
+            response = requests.post(f"{settings.DB_URL}/hub/add", data=json_code)
+
+            if response.status_code == 200:
+                msg = "Congratulations your script has been accepted to be apart of the script hub. Give up to 24 hours to be update on the website"    
+            else:
+                msg = "There has been an internal server error this means that the revival severs are currently not functional"
+
+            await channel.send("Congratulations your script has been accepted to be apart of the script hub. Give up to 24 hours to be update on the website")
+
+        elif reaction[0].emoji == "🚫":
+            await channel.send(
+                "I'm sorry to tell you that your script that you submitted has been denied. This is most likely because you didn't give a proper Name, description or code"
+            )
+        else:
+            await channel.send(
+                f"The staff thought your submission was so bad they reacted with {reaction[0].emoji}"
+            )
+
---
+++ events/check_version.py
@@ -0,0 +1,39 @@
+from events.base_event import BaseEvent
+from utils import get_channel
+import requests as req
+import discord
+
+# Your friendly example event
+# You can name this class as you like, but make sure to set BaseEvent
+# as the parent class
+class CheckVersion(BaseEvent):
+    def __init__(self):
+        interval_minutes = 15  # Set the interval for this event
+        super().__init__(interval_minutes)
+
+    # Override the run() method
+    # It will be called once every {interval_minutes} minutes
+    async def run(self, client):
+        with open("version.txt", "r") as f:
+            version = f.read()
+            res = req.get("https://clientsettings.roblox.com/v1/client-version/WindowsPlayer")
+            latest_version = res.json()["clientVersionUpload"]
+            if version == latest_version:
+                pass
+            else:
+                channel = get_channel(client, "〖📌〗announcements")
+                msg = "<@!706549422968012812>"
+                await channel.send(msg)
+                embedVar = discord.Embed(title="**New Roblox Version Detected**", color=0x10b1fe, description="This means that revival and all other exploits will be temporally patched as we need to update our custom api. This shouldn't take long to update")
+                embedVar.add_field(name="Old Version", value=f"`{version}`", inline=True)
+                embedVar.add_field(name="New Version", value=f"`{latest_version}`", inline=True)
+                await channel.send(embed=embedVar)
+                with open("version.txt", "w") as j:
+                    j.write(latest_version)
+
+                print(version)
+                print(latest_version)
+
+
+
+            
