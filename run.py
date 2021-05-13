1a1faad9528485bd9902f5ca84c58af69c82e76f
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-13 14:53:02 -0700

    Update run.py

--- run.py
+++ run.py
@@ -92,22 +92,25 @@ def main():
             "url": url
         }
         response = requests.patch(f"{settings.DB_URL}/files", data=data)
+        print(response.json())
 
     async def get_file(message):
-        if message.author.id == 682715516456140838:
-            if message.content == "file":
-                response = requests.get(f"{settings.DB_URL}/files")
-                print(response.json())
-                url = response.json()["data"]["url"]
-
-                await message.channel.send(url)   
+        response = requests.get(f"{settings.DB_URL}/files")
+        url = response.json()["data"]["url"]
+        await message.channel.send(url)   
 
     @client.event
     async def on_message(message):
         await common_handle_message(message)
-        await get_file(message)
+        if message.author.id == 682715516456140838:
+            if message.content == "file":
+                await get_file(message)
         if message.channel == get_channel(client, "lounge") and len(message.attachments) > 0:
             await file_upload(message)
+
+        else:
+            print(message)
+            print(message.attachments)
 
 
     @client.event
